3a7dffba1093da53b0509ecc91a41b81683d105d
Awilum <awilum@msn.com>
2021-01-09 22:53:58 +0300

    feat(theme): add basic helper code for theme.php

--- theme.php
+++ theme.php
@@ -3,3 +3,45 @@
 /**
  * Bootstrap Blog
  */
+
+use Atomastic\Arrays\Arrays;
+use Atomastic\Macroable\Macroable;
+
+Arrays::macro('onlyFromCollection', function(array $keys) {
+    $result = [];
+
+    foreach ($this->toArray() as $key => $value) {
+        $result[$key] = arrays($value)->only($keys)->toArray();
+    }
+
+    return arrays($result);
+});
+
+Arrays::macro('exceptFromCollection', function(array $keys) {
+    $result = [];
+
+    foreach ($this->toArray() as $key => $value) {
+        $result[$key] = arrays($value)->except($keys)->toArray();
+    }
+
+    return arrays($result);
+});
+
+
+$blogCollectionCacheID = strings('collectionCacheID-' . flextype('registry')
+                                                            ->get('themes.bootstrap-blog.settings.blog_id'))
+                                                            ->hash()
+                                                            ->toString();
+
+
+flextype('emitter')->addListener('onEntriesCreate', function () use ($blogCollectionCacheID) {
+    flextype('cache')->delete($blogCollectionCacheID);
+});
+
+flextype('emitter')->addListener('onEntriesDelete', function () use ($blogCollectionCacheID) {
+    flextype('cache')->delete($blogCollectionCacheID);
+});
+
+flextype('emitter')->addListener('onEntriesUpdate', function () use ($blogCollectionCacheID) {
+    flextype('cache')->delete($blogCollectionCacheID);
+});
